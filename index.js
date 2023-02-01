b2d797aeadd34cb5792b3b83ba1bc25d97fe61d0
acej0k3r <ace.rav3n@gmail.com>
2023-02-01 01:24:13 -0500

    f

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const startserver = async () => {
 
     try {
         connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+        app.listen('https://artilligence.onrender.com', () => console.log('Server has started on port http://localhost:8080'))
     } catch (error) {
         console.log(error);
 
